123e32306ad3bcd6d829faa22c8fa42a7a769e22
Chris Brown <code@drbyte.dev>
2024-09-17 02:59:20 -0400

    Remove extra directory

--- zc_plugins/MAP-Pricing/v3.0.0/Installer/languages/french/main.php
+++
@@ -1,9 +0,0 @@
-<?php
- // MAP Pricing by SlickRicky Design : http://www.slickricky.com/
-
-$define = [
-    'ADMIN_PLUGIN_MANAGER_NAME_FOR_MAP_PRICING' => "PMA Prix Minimum Annoncé",
-    'ADMIN_PLUGIN_MANAGER_DESCRIPTION_FOR_MAP_PRICING' => "Ce plugin permet à votre site de masquer les prix à moins que le client ne se connecte ou d'afficher uniquement des prix meilleurs que ceux annoncés après avoir ajouté un produit au panier.",
-];
-
-return $define;
